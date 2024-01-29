c344ed43ba0319188aff6f6e8520be9d03196476
tqmvt <66651502+tqmvt@users.noreply.github.com>
2024-01-28 16:40:06 -0800

    :dragon: add dapp starter :dragon:

---
+++ src/appConfig.ts
@@ -0,0 +1,12 @@
+export const app: { [key: string]: string } = {
+  name: 'dapp-rainbow-starter', // web app name
+  title: 'dapp-rainbow-starter', // website title
+  description: 'dapp-rainbow-starter by https://github.com/tqmvt', // description of website
+  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
+  url: 'https://github.com/tqmvt', // website url
+  image: '/icon-512x512.png', // image url for SEO
+  favicon: '/favicon.ico', // favicon url
+  twitter: '@_scentforest', // twitter account
+  themeColor: '#FFFFFF', // theme color
+  themeColorDark: '#000000', // theme color dark
+}
